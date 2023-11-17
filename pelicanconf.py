cf33f3609f88cc74cdca53e4c6bc522e0f62997e
DrSaadLa <data16scientist@gmail.com>
2023-11-17 18:11:41 +0300

    Quick test

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,30 +3,8 @@ SITENAME = 'PyVersity'
 SITEURL = ''
 
 PATH = 'content'
-PAGE_PATHS = ['pages']
-ARTICLE_PATHS = ['posts']
- 
+
 TIMEZONE = 'Europe/Rome'
-
-THEME = 'site-theme/pelican-bootstrap3'
-BOOTSTRAP_THEME = 'yeti'
-
-PLUGIN_PATHS = ['pelican-plugins']
-JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
-PLUGINS = [
-    'i18n_subsites',
-    'series',
-    'tag_cloud',
-    #'liquid_tags.youtube',
-    #'liquid_tags.notebook',
-    #'liquid_tags.include_code',
-    'render_math',
-    #'pelican-ipynb.markup',
-    #'tipuesearch'
-]
-
-# for Tique Search Plugin
-DIRECT_TEMPLATES = ('index','tags', 'categories', 'authors', 'archives', 'search')
 
 DEFAULT_LANG = 'en'
 
